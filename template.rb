1f07fa94583d83a8f9694336544543cc3b3e3148
Rob Zolkos <rob@zolkos.com>
2023-12-12 20:20:54 -0500

    Add host to template

--- template.rb
+++ template.rb
@@ -2,4 +2,8 @@ inside 'config' do
   gsub_file 'database.yml', 'database: workspace_development', "database: #{ENV['APP_NAME']}_development"
   gsub_file 'database.yml', 'database: workspace_test', "database: #{ENV['APP_NAME']}_test"
   gsub_file 'database.yml', 'database: workspace_production', "database: #{ENV['APP_NAME']}_production"
+
+  database_config = File.read('database.yml')
+  database_config.gsub!(/adapter: postgresql\n/, "adapter: postgresql\n  host: <%= ENV.fetch('DB_HOST', 'localhost') %>\n")
+  File.open('database.yml', 'w') { |file| file.puts database_config }
 end
