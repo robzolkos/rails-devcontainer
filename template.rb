ae3aeeaa3c2844bc94aba1c39101a50695f48ff3
Rob Zolkos <rob@zolkos.com>
2023-12-12 20:25:17 -0500

    Add username to template

--- template.rb
+++ template.rb
@@ -4,6 +4,6 @@ inside 'config' do
   gsub_file 'database.yml', 'database: workspace_production', "database: #{ENV['APP_NAME']}_production"
 
   database_config = File.read('database.yml')
-  database_config.gsub!(/adapter: postgresql\n/, "adapter: postgresql\n  host: <%= ENV.fetch('DB_HOST', 'localhost') %>\n  password: postgres\n")
+  database_config.gsub!(/adapter: postgresql\n/, "adapter: postgresql\n  host: <%= ENV.fetch('DB_HOST', 'localhost') %>\n  password: postgres\n  username: postgres\n")
   File.open('database.yml', 'w') { |file| file.puts database_config }
 end
